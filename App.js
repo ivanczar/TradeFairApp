e1875bf90f9048bb9af07770b5bf181bca151e22
Ivan Czar <ivanczar2013@gmail.com>
2022-10-21 11:27:35 +1300

    fix: preventingg future merge conflicts

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import {
-  Text,
-  Box,
+
   NativeBaseProvider,
-  Button
+
 } from "native-base";
-import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
+
 
 export default function App() {
   return (
     <NativeBaseProvider>
-<LoginScreen/>
+
     </NativeBaseProvider>
   );
 }
